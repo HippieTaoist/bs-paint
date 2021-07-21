44e382e2d5caad8ed90d69f85c359b5e338fa6ef
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-07-20 21:59:38 -0600

    Adjust layout to match demo

--- main.js
+++ main.js
@@ -72,6 +72,54 @@ while (count <= gridWidth * gridWidth) {
 // square and for each palette color from the functions you
 // wrote above.
 
+
+
+// =========FLEX IT UP=========
+const app = document.querySelector('.app')
+    // app.style.justifyContent = 'space-evenly'
+    // app.style.alignContent = 'space-apart'
+app.style.flexDirection = 'column'
+app.style.border = 'red 3px solid'
+
+const headings = document.querySelector('.headings')
+headings.style.justifyContent = 'center'
+headings.style.flexDirection = 'column'
+headings.style.height = 'fit-content'
+headings.style.margin = 'auto'
+headings.style.textAlign = 'center'
+headings.style.border = 'blue 3px solid'
+
+
+
+const paint = document.querySelector('.paint')
+paint.style.justifyContent = 'center'
+paint.style.alignContent = 'space-evenly'
+paint.style.flexDirection = 'column'
+paint.style.margin = 'auto'
+paint.style.height = 'fit-content'
+paint.style.border = 'purple 3px solid'
+
+const palette = document.querySelector('.palette palette-color')
+palette.style.border = 'yellow 3px solid'
+palette.style.justifyContent = 'center'
+
+const brushSelection = document.querySelector('.brush-selection')
+    // brushSelection.style.justifyContent = 'center'
+    // brushSelection.style.alignContent = '
+brushSelection.style.margin = 'auto'
+brushSelection.style.flexDirection = 'column'
+brushSelection.style.border = 'green 1px solid'
+
+const canvasDisplay = document.querySelector('.canvas')
+canvasDisplay.style.justifyContent = 'center'
+canvasDisplay.style.alignContent = 'end'
+    // canvasDisplay.style.flexDirection = 'column'
+
+
+
+
+
+
 // Pallette Coloring
 let palletBox1 = document.querySelector('.color-1')
 let palletBox2 = document.querySelector('.color-2')
@@ -80,19 +128,51 @@ let palletBox4 = document.querySelector('.color-4')
 let palletBox5 = document.querySelector('.color-5')
 
 // Set Pallette
-
-
 function setPalette(choice) {
     if (choice === 1) {
         palletBox1.style.backgroundColor = 'red'
+        palletBox2.style.backgroundColor = 'orange'
+        palletBox3.style.backgroundColor = 'yellow'
+        palletBox4.style.backgroundColor = 'green'
+        palletBox5.style.backgroundColor = 'blue'
 
     }
 
 }
 setPalette(1)
-
-
-
 let CurrentPaintColor = document.querySelector('.current-brush')
 
+
+// Color Choice
+let colorChoice = document.querySelector('.palette')
 console.log(CurrentPaintColor)
+
+colorChoice.addEventListener('click', function(event) {
+    CurrentPaintColor.className = ('current-brush')
+    let tempPaint = event.target
+    let tempPaintNum = tempPaint.className[tempPaint.className.length - 1]
+    CurrentPaintColor.classList.add(`.color-${tempPaintNum}`)
+    console.log(CurrentPaintColor)
+})
+console.log(CurrentPaintColor)
+
+
+
+
+
+// Paint on Canvas
+let canvas = document.querySelector('.square')
+console.log(canvas)
+
+
+
+// canvas.forEach((square, index) => {
+//     square.addEventListener('click', function() { console.log(square, index) })
+// })
+
+// canvas.forEach(function(square, index) => {
+
+//     canvas.addEventListener('click', function() {
+//         console.log(square, index)
+//     });
+// });
